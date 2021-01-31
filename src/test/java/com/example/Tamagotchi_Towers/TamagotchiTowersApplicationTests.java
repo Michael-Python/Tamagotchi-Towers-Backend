161091e3a4557a9a5055ca3cf9101d36440046b2
StuuGraham <stuartgraham7@hotmail.co.uk>
2021-01-31 16:12:33 +0000

    Project created using Spring Initializer and application.properties loaded in

---
+++ src/test/java/com/example/Tamagotchi_Towers/TamagotchiTowersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tamagotchi_Towers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamagotchiTowersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
